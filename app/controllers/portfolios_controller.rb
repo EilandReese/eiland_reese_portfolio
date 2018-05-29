eb4c9c3085df26941a7c4209795ae28994c8db0a
Eiland S. Reese <eiland@eilandreese.com>
2018-05-29 18:26:59 -0500

    Finishes carrierwave installation

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,18 +25,16 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    def create
-      @portfolio_item = Portfolio.new(portfolio_params)
+    @portfolio_item = Portfolio.new(portfolio_params)
 
-      respond_to do |format|
-        if @portfolio_item.save
-          format.html { redirect_to portfolios_path, notice: 'Your portfolio item was successfully created.' }
-          format.json { render :show, status: :created, location: @portfolio_item }
-        else
-          format.html { render :new }
-          format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
-        end
-      end
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item was successfully created.' }
+        format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+       end
     end
   end
 
